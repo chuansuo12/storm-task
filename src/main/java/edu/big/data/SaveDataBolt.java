6adce5c49a0b97e70edac5ad4ea98a7a36b81d93
tengyj <tengyj@taikanglife.com>
2020-06-11 21:01:18 +0800

    增加local cluster

--- src/main/java/edu/big/data/SaveDataBolt.java
+++ src/main/java/edu/big/data/SaveDataBolt.java
@@ -23,22 +23,21 @@ import java.util.Map;
 
 public class SaveDataBolt implements IRichBolt {
     private static final byte[] USER_INFO = Bytes.toBytes("user_info");
-    private static final byte[] ARTICLE_INFO = Bytes.toBytes("article_info");
     private static final byte[] BEHAVIOR = Bytes.toBytes("behavior");
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.0");
+    private static final DateTimeFormatter HBASE_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    private static final String SEPARATOR = "_";
 
     private Table userBehaviorTable;
-    private Table articleBehaviorTable;
 
     public void prepare(Map<String, Object> map, TopologyContext topologyContext, OutputCollector outputCollector) {
         try {
             Configuration conf = HBaseConfiguration.create();
             Connection conn = ConnectionFactory.createConnection(conf);
             TableName userBehavior = TableName.valueOf("bigdata:user_behavior");
-            TableName articleBehavior = TableName.valueOf("bigdata:article_behavior");
             this.userBehaviorTable = conn.getTable(userBehavior);
-            this.articleBehaviorTable = conn.getTable(articleBehavior);
-	    System.out.println("connect hbase success!");
+            System.out.println("connect hbase success!");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -46,12 +45,10 @@ public class SaveDataBolt implements IRichBolt {
 
     public void execute(Tuple tuple) {
         try {
-	    //System.out.println("execute...");
+            //System.out.println("execute...");
             UserBehavior userBehavior = getUserBehavior(tuple);
-            Put userPut = this.getUserBehaviorPut(userBehavior);
-            Put articlePut = this.getArticleBehaviorPut(userBehavior);
-            userBehaviorTable.put(userPut);
-            articleBehaviorTable.put(articlePut);
+            Put userBehaviorPut = this.getUserBehaviorPut(userBehavior);
+            userBehaviorTable.put(userBehaviorPut);
             //System.out.println("save success, uid:{" + userBehavior.getUid() + "}, aid:{" + userBehavior.getAid() + "}");
         } catch (IOException e) {
             e.printStackTrace();
@@ -59,20 +56,15 @@ public class SaveDataBolt implements IRichBolt {
     }
 
     private Put getUserBehaviorPut(UserBehavior userBehavior) {
-        Put put = new Put(Bytes.toBytes(userBehavior.getUid()));
-        put.addColumn(ARTICLE_INFO, UserBehavior.AID_FILE, Bytes.toBytes(userBehavior.getAid()));
+        Put put = new Put(Bytes.toBytes(
+                userBehavior.getUid() + SEPARATOR
+                        + userBehavior.getAid() + SEPARATOR
+                        + userBehavior.getBehaviorTime().format(HBASE_DATE_TIME_FORMATTER)));
         put.addColumn(BEHAVIOR, UserBehavior.BEHAVIOR_FILED, Bytes.toBytes(userBehavior.getBehavior()));
         put.setTimestamp(userBehavior.getBehaviorTime().atZone(ZoneOffset.systemDefault()).toInstant().toEpochMilli());
         return put;
     }
 
-    private Put getArticleBehaviorPut(UserBehavior userBehavior) {
-        Put put = new Put(Bytes.toBytes(userBehavior.getAid()));
-        put.addColumn(USER_INFO, UserBehavior.UID_FILED, Bytes.toBytes(userBehavior.getUid()));
-        put.addColumn(BEHAVIOR, UserBehavior.BEHAVIOR_FILED, Bytes.toBytes(userBehavior.getBehavior()));
-        put.setTimestamp(userBehavior.getBehaviorTime().atZone(ZoneOffset.systemDefault()).toInstant().toEpochMilli());
-        return put;
-    }
 
     private UserBehavior getUserBehavior(Tuple tuple) {
         String value = tuple.getStringByField("value");
@@ -88,11 +80,6 @@ public class SaveDataBolt implements IRichBolt {
     public void cleanup() {
         try {
             userBehaviorTable.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            articleBehaviorTable.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
